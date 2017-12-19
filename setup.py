0e9b5479e0bd0faca892145594ac2cfea780416d
Jonathan <jonathan@saharacluster.com>
2017-12-19 20:15:53 +0300

    Push to Pypi

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+import os
+
+def read(fname):
+    """Utility function to get README.rst into long_description.
+
+    ``long_description`` is what ends up on the PyPI front page.
+    """
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+  name = 'emoji-flags',
+  packages = ['emoji-flags'],
+  version = '0.1.0',
+  description = 'Python Package for Country Flag Emojis.',
+  author = 'Jonathan Kosgei',
+  author_email = 'jonathan@ipdata.co',
+  url = 'https://github.com/jonathan-kosgei/emoji-flags',
+  download_url = 'https://github.com/jonathan-kosgei/emoji-flags/archive/0.1.0.tar.gz',
+  keywords = ['geolocation', 'flags', 'emoji'],
+  long_description=read('README'),
+  classifiers = [],
+)
