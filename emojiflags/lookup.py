610d19bf60ac305c16cdd5300a7c6c2a460f1dee
Alexandar Mechev <dev@apmechev.com>
2022-11-26 11:21:28 +0100

    ✨feat: add reverse lookup
    ex 🇦🇩 -> Andorra

--- emojiflags/lookup.py
+++ emojiflags/lookup.py
@@ -1,10 +1,19 @@
 from .data import flags
 
 data = {}
+reverse_data = {}
+
 for flag in flags:
     data[flag['code']] = flag['emoji']
+    reverse_data[str(flag['emoji'])] = flag['code']
 
 def lookup(country_code):
     if country_code in data:
         return data[country_code]
     return None
+
+def reverse_lookup(emoji):
+    if emoji in reverse_data:
+        return reverse_data[emoji]
+    return None
+
